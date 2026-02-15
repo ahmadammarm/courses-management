c4896939c1237993733414736a60b641985f328e
ahmadammarm <ahmad.ammar.2205356@students.um.ac.id>
2026-02-15 17:47:22 +0700

    feat(backend): add jwt password hashing utlity

---
+++ backend/utils/passwordhash.go
@@ -0,0 +1,12 @@
+package utils
+
+import "golang.org/x/crypto/bcrypt"
+
+func PasswordHash(password string) string {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return ""
+	}
+
+	return string(hashedPassword)
+}
